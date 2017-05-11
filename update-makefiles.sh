449ac3df2769016cb1b69d78d493e87e3ec56531
Steven Moreland <smoreland@google.com>
2017-05-11 15:42:32 -0700

    Update makefiles from anywhere.
    Test: with update-all-google-makefiles.sh
    Change-Id: I373e270b481d9074f440c3c0826199a207e654f7
    
    run vts build rule updater when hal make files are updated
    
    Test: ./update-makefiles.sh
    Bug: 117826861
    Change-Id: I7c7b22ecff1a373cd905fedd96bdaa12e1a558f0

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# Script to update Android make-files for HAL and VTS modules.
 
-source system/tools/hidl/update-makefiles-helper.sh
+set -e
+
+if [ -z "$ANDROID_BUILD_TOP" ]; then
+    echo "Missing ANDROID_BUILD_TOP env variable. Run 'lunch' first."
+    exit 1
+fi
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
   "vendor.evervolv:hardware/evervolv/interfaces"
